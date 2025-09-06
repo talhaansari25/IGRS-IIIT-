1a9028bb954e1d7100664e6e9c02882ace1c9d9f
Talha <talhaansari2026@gmail.com>
2025-09-06 20:42:39 +0530

    .

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,3 +1,4 @@
+// App. jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminDashboard from './pages/AdminDashboard';
 import StatusPage from './pages/StatusPage';
